36d5c56daf315a370cc7fca70148076c53662454
daan <daniel@bitofpepper.com>
2020-04-29 12:58:04 +0200

    first commit

---
+++ discuss/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from discuss.users.models import User
+from discuss.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
